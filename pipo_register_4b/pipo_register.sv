80418139f69044a9e4ca23dfa3e8bc776246217b
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2024-11-16 23:50:37 +0530

    Create pipo_register.sv

---
+++ pipo_register_4b/pipo_register.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module pipo_register (
+    input logic clk,           
+    input logic reset,         
+    input logic load,          
+    input logic [3:0] data_in, 
+    output logic [3:0] data_out 
+);
+
+    reg [3:0] register;
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+        
+            register <= 4'b0000; 
+            
+        end else if (load) begin
+          
+            register <= data_in; 
+        
+        end
+    
+    end
+
+    assign data_out = register;
+
+endmodule
