550036a8df120da9bdadd8ead9e6915159c27674
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2024-11-16 22:54:11 +0530

    Update mux_2_1.sv

--- mux_2_1/mux_2_1.sv
+++ mux_2_1/mux_2_1.sv
@@ -9,3 +9,33 @@ module mux_2_1
 );
     assign y = s ? d1 : d0;
 endmodule
+
+
+// Using tri state buffer -----------------------------------------------------------------------------------------------------------------------------------------
+
+ // logic buf0_out, buf1_out;
+
+   
+   // tristate_buffer buf0 (
+        // .data_in(D0),
+       // .enable(~S),      
+      // .data_out(buf0_out)
+   // );
+
+    // tristate_buffer buf1 (
+       // .data_in(D1),
+       // .enable(S),        
+       // .data_out(buf1_out)
+   // );
+
+//    assign Y = (buf0_out !== 1'bz) ? buf0_out : buf1_out;
+
+// endmodule
+
+// module tristate_buffer (
+    // input logic data_in,      
+    // input logic enable,       
+    // output logic data_out     
+// );
+    // assign data_out = (enable) ? data_in : 1'bz; 
+// endmodule
